166b05bef493e713209984c7368f4bae21e3be1f
ifuji <ifujihaku1216@gmail.com>
2026-01-29 19:08:01 +0900

    divをまわしてます

---
+++ web/src/app/haku/page.tsx
@@ -0,0 +1,12 @@
+
+
+function Page(){
+    return(
+        <div>
+            <div className="font-bold m-12 animate-spin text-center">
+                div
+            </div>
+        </div>
+    );
+}
+export default Page;
