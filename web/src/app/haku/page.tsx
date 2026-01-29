21ce51434fda9760f384b39b05f1944608b9b874
けぃと° <142198827+katedegree@users.noreply.github.com>
2026-01-29 19:27:57 +0900

    Update page.tsx

--- web/src/app/haku/page.tsx
+++ web/src/app/haku/page.tsx
@@ -3,7 +3,7 @@
 function Page(){
     return(
         <div>
-            <div className="font-bold m-12 animate-spin text-center">
+            <div className="font-bold m-12 animate-spin text-center text-[100px]">
                 div
             </div>
         </div>
