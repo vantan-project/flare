4152b85c884588521798e5f6e5e30ad4f507f230
ifuji <ifujihaku1216@gmail.com>
2026-02-01 20:20:51 +0900

    余分な中括弧を取り除きました

--- web/src/components/footer/blog-footer.tsx
+++ web/src/components/footer/blog-footer.tsx
@@ -9,22 +9,22 @@ export function Footer() {
   return (
     <div>
       <div className="bg-white flex justify-evenly p-4">
-        <Link href={"/"}>
+        <Link href="/">
           <div className={cn(pathname == "/" && "text-primary")}>
             <Icon name="home" size={32} />
           </div>
         </Link>
-        <Link href={"/create"}>
+        <Link href="/create">
           <div className={cn(pathname == "/create" && "text-primary")}>
             <Icon name="add" size={32} />
           </div>
         </Link>
-        <Link href={"/bookMark"}>
+        <Link href="/bookMark">
           <div className={cn(pathname == "/bookmark" && "text-primary")}>
             <Icon name="book" size={32} />
           </div>
         </Link>
-        <Link href={"/profile"}>
+        <Link href="/profile">
           <div className={cn(pathname == "/profile" && "text-primary")}>
             <Icon name="profile" size={32} />
           </div>
