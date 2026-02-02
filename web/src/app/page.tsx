107e993e5a9c4d3bb967d101035fc24eeef83e2f
ifuji <ifujihaku1216@gmail.com>
2026-02-02 18:20:29 +0900

    home実装

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { BlogIndexResponse } from "@/lib/api/blog-index";
+import { BlogCard } from "@/components/blog-card/blog-card";
+import { BlogSideCard } from "@/components/blog-sidecard/blog-sidecard";
+import { Footer } from "@/components/footer/footer";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function () {
@@ -22,10 +26,108 @@ export default function () {
     ],
     updateAt: "60分前",
   }));
+  const dammyBlogs2 = Array.from({ length: 4 }, (_, i) => ({
+    id: i + 1,
+    title: `タイトル${i + 1}`,
+    thumbnailImageUrl: "https://placehold.jp/200x200.png",
+    user: {
+      id: i,
+      name: "テストユーザー",
+      iconImageUrl: "https://placehold.jp/300x200.png",
+    },
+    wishesCount: 23,
+    bookmarksCount: 23,
+    tags: [
+      { id: 1, name: "タグ" },
+      { id: 2, name: "タグ" },
+      { id: 3, name: "タグ" },
+    ],
+    updateAt: "60分前",
+  }));
 
   const [flareBlogs, setFlareRanking] = useState<BlogIndexResponse>(dammyBlogs);
   const [coreBlogs, setCoreRanking] = useState<BlogIndexResponse>(dammyBlogs);
-  const [newBlogs, setNewBlogs] = useState<BlogIndexResponse>(dammyBlogs);
+  const [newBlogs, setNewBlogs] = useState<BlogIndexResponse>(dammyBlogs2);
 
-  return <div></div>;
+  return (
+    <div>
+      <div className="p-5">
+        <div className="font-bold text-[16px] border-b-2 border-primary p-2">
+          高熱意度ピックアップ
+        </div>
+        <div className="p-3" />
+        <div className="flex gap-3 overflow-x-auto">
+          {flareBlogs.map((b) => (
+            <BlogCard
+              key={b.id}
+              title={b.title}
+              user={b.user}
+              wishedCount={b.wishesCount}
+              bookmarkedCount={b.bookmarksCount}
+              thumbnailImageUrl={b.thumbnailImageUrl}
+            />
+          ))}
+        </div>
+        <div className="m-5" />
+        <div className="font-bold text-[16px] border-b-2 p-2">
+          コア度セレクション
+        </div>
+        <div className="p-3" />
+        <div className="flex gap-3 overflow-x-auto">
+          {coreBlogs.map((b) => (
+            <BlogCard
+              key={b.id}
+              title={b.title}
+              user={b.user}
+              wishedCount={b.wishesCount}
+              bookmarkedCount={b.bookmarksCount}
+              thumbnailImageUrl={b.thumbnailImageUrl}
+            />
+          ))}
+        </div>
+        <div className="m-5" />
+        <div className="mb-3">
+          新着投稿
+          <Link href="https://www.youareanidiot.cc/" className="px-3">
+            もっと見る＞
+          </Link>
+        </div>
+        <div className="flex flex-col gap-3">
+          {newBlogs.map((b) => (
+            <BlogSideCard
+              key={b.id}
+              title={b.title}
+              user={b.user}
+              wishedCount={b.wishesCount}
+              bookmarkedCount={b.bookmarksCount}
+              thumbnailImageUrl={b.thumbnailImageUrl}
+            />
+          ))}
+        </div>
+        <div className="m-5" />
+        <div className="mb-3">
+          投稿一覧
+          <Link href="https://www.youareanidiot.cc/" className="px-3">
+            もっと見る＞
+          </Link>
+        </div>
+        <div className="flex flex-col gap-3">
+          {newBlogs.map((b) => (
+            <BlogSideCard
+              key={b.id}
+              title={b.title}
+              user={b.user}
+              wishedCount={b.wishesCount}
+              bookmarkedCount={b.bookmarksCount}
+              thumbnailImageUrl={b.thumbnailImageUrl}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="p-20" />
+      <div className="fixed bottom-0 w-full">
+        <Footer />
+      </div>
+    </div>
+  );
 }
