f979873a02ff80827d0f642d370d630057779ffe
Kate0418 <nakao.keito0418@gmail.com>
2026-02-05 11:07:25 +0900

    token修正

--- web/src/utils/access-token.ts
+++ web/src/utils/access-token.ts
@@ -1,8 +1,10 @@
 import Cookie from "js-cookie";
 
 const ACCESS_TOKEN_KEY = "accessToken";
+
 export const accessToken = {
   get: () => Cookie.get(ACCESS_TOKEN_KEY),
-  set: (value: string) => Cookie.set(ACCESS_TOKEN_KEY, value),
+  set: (value: string) =>
+    Cookie.set(ACCESS_TOKEN_KEY, value, { expires: 31 }),
   remove: () => Cookie.remove(ACCESS_TOKEN_KEY),
 };
